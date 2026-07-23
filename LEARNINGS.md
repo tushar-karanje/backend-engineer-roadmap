a8c0a60bd009001bd5542a7d229b3fc4d5604ee8
Tushar Karanje <tushar.kantilalkaranje.external@ynap.com>
2026-07-23 12:03:49 +0530

    Modified employee.py

--- LEARNINGS.md
+++ LEARNINGS.md
@@ -1,3 +1,67 @@
+Day 4:
+
+
+
+
+Day 3:
+
+Exercise 1:
+employees is a list object.
+Yes I can append,search and remove elements from list.
+If I append Tushar again, it will allow because list allows duplicate elements.
+
+Exercise 2:
+employees is a set object.
+Set is like list but the only difference is that set does not allow duplicates
+If we try to append "Tushar" again in set, it will not throw any error but it will not insert "Tushar" again as it is already present in employees
+
+
+Exercise 3:
+employees is a dict object.
+employees[2] will be 'Amit'
+
+Reason why searching in a dict is faster than lists is because list starts element from the 0th index and it has to gone through end if element is not present.
+If list is having small number of elements like 10-50 then we will not be able to see any difference but if list is having millions of records and if we provide any element which is either at last or not present , then search will be inefficient.
+Also, list allows duplicates, due to this also list become inefficient to search the elements
+
+
+
+
+Interview Questions
+Q1.
+
+Predict:
+Output : {'Tushar', 'Amit'}
+
+Reason:
+Because employees is the set object. 
+Sets does not allow duplicates.
+
+
+Q2.
+
+Predict:
+Output: 400000
+
+Complexity : average O(1) 
+Note : I don't know how to calculate the complexity. But yes I want to learn it.
+
+
+
+Complexity for 
+number.pop(0)
+
+I am bit confused here.
+I think here pop function will act as remove function but with index.
+So I believe, the complexity will be O(n) , because if we remove 0th element, all other elements needs to be placed again by reducing the index by 1. Due to which python needs to work a lot.
+
+Also another possibility is that pop(0) will remove first element. Hence, python knows the current size of list "numbers", so it may simply moves 0th for the next memory location
+
+
+
+ 
+
+
 
 Day 2:
 
--- employee.py
+++ employee.py
@@ -1,34 +1,85 @@
 import sys
-employees = ["Tushar","Mr X", "Hruday", "Amit", "Rushank"]
+
+employees = {
+    1: {
+        "name": "Tushar",
+        "department": "Engineering",
+        "salary": 4000000
+    },
+    2:{
+        "name": "Rushank",
+        "department": "Management",
+        "salary": 6000000       
+    },
+    3:{
+      "name": "Hruday",
+      "department": "Creative",
+      "salary": 8000000
+    }
+
+}
 
 def add_employee():
     employee_name = input("Enter name of employee: ")
-    employees.append(employee_name)
+    employee_dept = input("Enter department of employee: ")
+    employee_salary = input("Enter Salary of Employee: ")
+
+    employee_id = max(employees) + 1
+
+    employee_details =  {"name" : employee_name , "department": employee_dept, "salary":employee_salary}
+
+    employees[employee_id] = employee_details
+    #employees.append(employee_name)
     print(f"Employee {employee_name} added succesfully")
 
 def show_employees():
-    print(f"Here is the list of employees: {employees}")
+    for id,detail in employees.items():
+      print (f"ID : {id}")
+
+      for key,value in detail.items():
+         print(f"\t {key} : {value}")
+
 
 
 def search_employee():
-    search_name = input("Enter the name of Employee: ").strip()
-    if (search_name in employees):
-       print(f"Following employee is present in list: {search_name}")
+    try :
+      emp_id = int(input("Enter the ID which you want to search:").strip())
+    except ValueError :
+      print("Invalid input. Please enter a numerinc ID")
+
+    if (emp_id in employees):
+        print("Details of the requested employee are as follows:")
+        print(f"\nName of Employee: {employees[emp_id]['name']}" )
+        print(f"\nDepartment of Employee: {employees[emp_id]['department']}" )
+        print(f"\nSalary of Employee: {employees[emp_id]['salary']}")
     else :
-       print (f"Employee {search_name} is not present")
+       print (f"Employee {emp_id} is not present")
 
 def delete_employee():
-    emp_name = input("Enter the name which you want to delete:").strip()
-    if (emp_name in employees):
-        employees.remove(emp_name)
-        print(f"Removed employee {emp_name} so the updated list is : {employees}")
+    try :
+       emp_id = int(input("Enter the ID which you want to delete:").strip())
+    except ValueError :
+       print("Invalid input. Please enter a numeric ID.")
+       return
+    
+    if emp_id in employees:
+        print("Details of the requested employee are as follows:")
+        print(f"\nName of Employee: {employees[emp_id]['name']}" )
+        print(f"\nDepartment of Employee: {employees[emp_id]['department']}" )
+        print(f"\nSalary of Employee: {employees[emp_id]['salary']}")
+        confirmation = input("Are you sure to delete this employee? (Y: Yes) ").strip().upper()
+        if ( confirmation == 'Y' ):
+          del employees[emp_id]
+          print(f"Removed employee {emp_id} so the updated list is : {employees}")
+        else :
+           print("Not removed employee" )
     else :
-       print(f"Unknown Employee: {emp_name} ")
+       print(f"Unknown Employee: {emp_id} ")
 
 
 def main_menu():
   while (True):  
-    choice=input("Enter your choice: (Add : A, Delete: D, Show: S, Search/Check: C)").strip()
+    choice=input("Enter your choice: (Add : A, Delete: D, Show: S, Search/Check: C)").strip().upper()
 
     match choice:
       case "A":
