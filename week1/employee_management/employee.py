b208fb953e6b7e9374fa5dc163fc2f7c07e29f25
Tushar Karanje <tushar.kantilalkaranje.external@ynap.com>
2026-08-10 17:23:37 +0530

    Added changes in wrapper

--- week1/employee_management/employee.py
+++ week1/employee_management/employee.py
@@ -1,10 +1,11 @@
 import sys
-from EmployeeManager import employee_manager
+from EmployeeManager import EmployeeManager
+from EmployeeClass import Employee
 
 def main_menu():
-  app = employee_manager()
+  app = EmployeeManager()
   while (True):  
-    choice=input("Enter your choice: (Add : A, Delete: D, Show: S, Search/Check: C)").strip().upper()
+    choice=input("Enter your choice: (Add : A, Delete: D, Show: S, Search/Check: C, Increment Salary: I)").strip().upper()
 
     match choice:
       case "A":
@@ -19,6 +20,21 @@ def main_menu():
       case "C":
         app.search_employee()
         continue
+      case "I":
+        try :
+          emp_id = int(input("Enter the employee ID of whose you want to increment the salary:").strip())
+          percentage = int(input("Enter the percentage by which you want to increment the salary: ".strip()))
+        except ValueError :
+          print("Invalid input. Please enter a numeric ID")
+
+        try : 
+          employee = app.get_employee(emp_id)
+          employee.increment_salary(percentage)
+        except ValueError as e:
+          print(e)
+        except AttributeError as ae:
+          print(f"Employee ID {emp_id} does not exists.")
+
       case _:
         print("Unknown Choice. Exiting...")
         sys.exit()
